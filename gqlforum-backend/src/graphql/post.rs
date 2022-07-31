dbb54d51508cbfea1ec18695e30a87194397eba8
Rongcui Dong <rongcuid@outlook.com>
2022-07-30 20:44:06 -0400

    Better post structure

--- gqlforum-backend/src/graphql/post.rs
+++ gqlforum-backend/src/graphql/post.rs
@@ -5,29 +5,21 @@ use super::user::User;
 
 #[derive(SimpleObject, Clone)]
 pub struct Post {
-    pub meta: PostMeta,
+    pub post_number: i64,
+    pub created_at: PrimitiveDateTime,
+    pub updated_at: Option<PrimitiveDateTime>,
+    pub deleted_at: Option<PrimitiveDateTime>,
+    pub meta: Option<PostMeta>,
     pub content: Option<PostContent>,
 }
 
 impl<'r> FromRow<'r, SqliteRow> for Post {
     fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
-        let meta = PostMeta::from_row(row)?;
+        let meta = PostMeta::from_row(row).ok();
         let content = PostContent::from_row(row).ok();
-        Ok(Self { meta, content })
-    }
-}
-
-#[derive(SimpleObject, Debug, Clone)]
-pub struct PostMeta {
-    pub post_number: i64,
-    pub created_at: PrimitiveDateTime,
-    pub updated_at: Option<PrimitiveDateTime>,
-    pub deleted_at: Option<PrimitiveDateTime>,
-}
-
-impl<'r> FromRow<'r, SqliteRow> for PostMeta {
-    fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
         Ok(Self {
+            meta,
+            content,
             post_number: row.try_get("post_number")?,
             created_at: row.try_get("created_at")?,
             updated_at: row.try_get("updated_at")?,
@@ -37,27 +29,31 @@ impl<'r> FromRow<'r, SqliteRow> for PostMeta {
 }
 
 #[derive(SimpleObject, Debug, Clone)]
-pub struct PostContent {
+pub struct PostMeta {
     pub author: User,
+}
+
+impl<'r> FromRow<'r, SqliteRow> for PostMeta {
+    fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            author: User {
+                id: row.try_get("author_user_id")?,
+                name: row.try_get("username")?,
+                signature: row.try_get("post_signature")?,
+            },
+        })
+    }
+}
+
+#[derive(SimpleObject, Debug, Clone)]
+pub struct PostContent {
     pub body: String,
 }
 
 impl<'r> FromRow<'r, SqliteRow> for PostContent {
     fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
-        let id: Option<i64> = row.try_get("author_user_id")?;
-        let name: Option<String> = row.try_get("username")?;
-        let signature: Option<String> = row.try_get("post_signature")?;
         let body: Option<String> = row.try_get("body")?;
-        let f = || -> Option<Self> {
-            Some(Self {
-                author: User {
-                    id: id?,
-                    name: name?,
-                    signature,
-                },
-                body: body?,
-            })
-        };
+        let f = || -> Option<Self> { Some(Self { body: body? }) };
         f().ok_or(sqlx::Error::RowNotFound)
     }
 }
