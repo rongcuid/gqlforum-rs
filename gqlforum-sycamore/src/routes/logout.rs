9e0f11d93deaeabdae8bd2f6e38edd6e6007a286
Rongcui Dong <rongcuid@outlook.com>
2022-07-31 10:39:17 -0400

    Logout flow

--- gqlforum-sycamore/src/routes/logout.rs
+++ gqlforum-sycamore/src/routes/logout.rs
@@ -1,8 +1,55 @@
-use sycamore::prelude::*;
+use serde_json::Value;
+use sycamore::{prelude::*, suspense::Suspense};
+
+use crate::graphql::GraphQLClient;
+
+#[component]
+async fn LogoutOutput<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let client = use_context::<GraphQLClient>(cx);
+    let resp = client
+        .query_raw(
+            r#"
+                    mutation {
+                        logout
+                    }
+                    "#,
+        )
+        .await
+        .unwrap();
+    let errors = create_signal(cx, Vec::new());
+    view! {cx, (
+        if let Some(errs) = &resp.errors {
+            errors.set(errs.iter().map(|x| x.message.clone()).collect());
+            view! { cx,
+                ul {
+                    Indexed {
+                        iterable: errors,
+                        view: |cx, x| view! { cx,
+                            li { (x) }
+                        }
+                    }
+                }
+            }
+        } else if let Some(data) = &resp.data {
+            if (|| {
+                Some(data.get("logout")? )
+            })() == Some(&Value::Bool(true)) {
+                view! {cx, p {"Logged out!"} }
+            } else {
+                view! {cx, p {"Authentication error!"}}
+            }
+        } else {
+            view! {cx, p {"Internal Server Error"}}
+        }
+    )}
+}
 
 #[component]
 pub fn Logout<G: Html>(cx: Scope<'_>) -> View<G> {
     view! { cx,
-        "Stub logout"
+        Suspense {
+            fallback: view! { cx, "Logging out..." },
+            LogoutOutput { }
+        }
     }
 }
